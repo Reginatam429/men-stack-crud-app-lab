db3c9ac16388c4fc228dfc0586702bd1952b44b3
Regina <reginatam429@gmail.com>
2025-03-11 03:04:36 -0700

    fixed css

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const dotenv = require("dotenv");
-const taskRoutes = require("./routes/taskRoutes");
 
 dotenv.config();
 const app = express();
 
 // Middleware
+app.use(express.static("public")); // Make sure your "public" folder exists
 app.set("view engine", "ejs");  // Set EJS as template engine
 app.use(express.urlencoded({ extended: true }));  // Body parser
-app.use(methodOverride("_method"));  
+app.use(methodOverride("_method"));
+
+// Import Routes (Move this ABOVE `app.use("/tasks", taskRoutes)`)
+const taskRoutes = require("./routes/taskRoutes");
+
+// Use Routes AFTER importing
 app.use("/tasks", taskRoutes);
 
 // Connecting to MongoDB
@@ -20,15 +25,10 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log("MongoDB Connected to Student Cluster"))
     .catch(err => console.log(err));
 
-
 // Home Route
 app.get("/", (req, res) => {
     res.render("home");
 });
-
-// Task Routes
-app.use("/tasks", taskRoutes);
-
 
 // Start Server
 const PORT = process.env.PORT || 3000;
